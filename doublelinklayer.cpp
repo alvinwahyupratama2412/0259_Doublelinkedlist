f42780dfa6f8aa26607daa960c54c0f4b5e3033a
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-16 08:58:02 +0700

    mencari node yang ingin di hapus dan  mengeceknya

--- doublelinklayer.cpp
+++ doublelinklayer.cpp
@@ -73,7 +73,12 @@ public:
 
         Node *current = START;
         
-        
+        while(current != NULL && current->noMhs != rollNo)
+        current = current->next;
+        if (current==NULL){
+            cout << "Record not found"<< endl;
+            return;
+        }
     }
 
 };
