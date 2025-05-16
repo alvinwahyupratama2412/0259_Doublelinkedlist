4d8e0b12ade40b5145c8d343af3663ef42183a34
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-16 09:14:51 +0700

    menampilkan seluruh data mahasiswa secara ascending

--- doublelinklayer.cpp
+++ doublelinklayer.cpp
@@ -103,6 +103,15 @@ public:
         }
 
         Node *currentNode = START;
+
+        cout << "\nRecords in ascending order of roll number are: \n";
+        int i = 0 ;
+        while(currentNode != NULL){
+            cout << i + 1 << ". " << currentNode->noMhs<<" "<< endl;
+
+            currentNode = currentNode->next;
+            i++;
+        }
     }
 
 };
